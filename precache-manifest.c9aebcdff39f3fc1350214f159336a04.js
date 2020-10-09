492d99b2f9fb6d6b65df5389ca09e23c30dd7ba2
amiri-zakiye <prsv865@gmail.com>
2020-10-09 07:36:56 -0700

    Updates

---
+++ precache-manifest.c9aebcdff39f3fc1350214f159336a04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e6d3c34e15f26f7033be4cab8c38b3",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "ced9e6b578f68175d374",
+    "url": "/crud/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "689d35519a99ed944ed6",
+    "url": "/crud/static/js/2.05556ac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crud/static/js/2.05556ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced9e6b578f68175d374",
+    "url": "/crud/static/js/main.9a128fae.chunk.js"
+  },
+  {
+    "revision": "3d2107f09fcddc21becb",
+    "url": "/crud/static/js/runtime-main.49a9eee0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crud/static/media/logo.5d5d9eef.svg"
+  }
+]);
